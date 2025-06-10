2702713816769b96e4061ba2705028652ca86a02
Marcvg69 <marc.vangoolen@telenet.be>
2025-06-10 12:45:06 +0200

    Test with 4 players

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from utils.game import Board
 # Create 2 players
 p1 = Player("Marc")
 p2 = Player("Ella")
+p3 = Player("Vincent")
+p4 = Player("Maxim")
 
-_players = [p1, p2]
+_players = [p1, p2, p3, p4]
 
 """
 Create the board and start the game 
