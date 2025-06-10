b7283dda0171cf27997369bed824a8f3a2594a31
Marcvg69 <marc.vangoolen@telenet.be>
2025-06-10 11:51:33 +0200

    Create Board class

---
+++ utils/game.py
@@ -0,0 +1,87 @@
+from utils.card import Card
+from utils.player import Player, Deck
+
+
+class Board:
+    """
+    Board class: Represents the board where the game is played. It manages the game flow, players, and cards.
+    
+    Attributes:
+    :players: A list of Player objects representing all the players participating in the game.
+    :turn_count: The current turn number in the game.
+    :active_cards: A list of cards that have been played in the current hand (round).
+    :history_cards: A list of all the cards that have been played throughout the entire game.
+    """
+
+    # List of players in the game
+    players: list[Player] = []
+    # Track the current turn count
+    turn_count: int = 0
+    # List of cards that are active (played in the current round)
+    active_cards: list[Card] = []
+    # List of cards that have been played in previous turns
+    history_cards: list[Card] = []
+
+    def __init__(self, players: list[Player]):
+        """
+        Constructor for the Board class.
+        
+        Args:
+        :players: A list of Player objects that will play the game.
+        """
+        self.players = players  # Initialize the players list
+
+    def __str__(self):
+        """
+        String representation of the Board object, showing the number of players and the current turn number.
+        """
+        return len(self.players) + " players at turn nr: " + str(self.turn_count)
+
+    def start_game(self):
+        """
+        Starts the game by:
+        1. Creating a Deck object.
+        2. Filling the deck with 52 cards.
+        3. Shuffling the deck to randomize the order of cards.
+        4. Distributing 13 cards to each player.
+        5. For each turn, the players play their cards, which are removed from the card history.
+        
+        The game proceeds for 13 turns, as each player has 13 cards.
+        """
+        
+        # Create a Deck object
+        _deck = Deck()
+        
+        # Fill the deck with 52 cards (using fill_deck method)
+        _deck.fill_deck()
+        
+        # Shuffle the cards to randomize the deck
+        _deck.shuffle()
+        
+        # Distribute the shuffled cards to the players (13 cards each)
+        _deck.distrubute(self.players)
+        
+        # Initialize the history_cards to be the cards in the deck
+        self.history_cards = _deck
+
+        # Loop over 13 turns (since each player has 13 cards)
+        for t in range(0, 13):
+            print("____________________________")
+            # Print the current turn number and remaining cards in the deck
+            print(
+                "Turn: "
+                + str(t + 1)
+                + " / Cards Left: "
+                + str(len(self.history_cards.cards))
+            )
+
+            # Loop through each player and have them play a card
+            for k in self.players:
+                # The player plays a card
+                _k = k.play()
+                
+                # Remove the played card from the history of cards
+                self.history_cards.cards.remove(_k)
+                
+                # Add the played card to the active cards list
+                self.active_cards.append(_k)
--- utils/player.py
+++ utils/player.py
@@ -1,35 +1,126 @@
 from utils.card import Card
 from typing import List
-import random #import random to shuffle the cards and chose a random card 
+import random  # Importing random to shuffle the cards and choose a random card
 
 class Player:
     """
-    Player class:
-    :name = name of the player
-    :cards = the cards in player's hand
-    :turn count= which turn is getting played
-    :number of cards: how manycards does player have in hand
-    :history= cards he played
+    Player class: Represents a player in the card game.
+    
+    Attributes:
+    :name: The name of the player.
+    :cards: A list of cards the player currently holds (player's hand).
+    :turn_count: The turn number to track which turn is being played (i.e., which player's turn).
+    :number_of_cards: The number of cards the player has left in their hand.
+    :history: A list of cards the player has played during the game.
     """
 
+    # List of cards the player currently holds
     cards: List[Card] = []
+    # Track the turn number
     turn_count: int = 0
+    # Each player starts with 13 cards
     number_of_cards: int = 13
+    # Keep track of the cards the player has played
     history: List[Card] = []
 
     def __init__(self, name: str):
+        """
+        Constructor for Player class.
+        
+        Args:
+        :name: The name of the player.
+        """
         self.name = name
 
     def play(self):
         """
-        Function to play a card. Played cards removed from cards and added to history
-        also keeping turn number for future reference
+        Function to play a card. When a player plays a card:
+        - A random card is chosen from the player's hand (cards).
+        - The card is removed from the player's hand and added to the history of played cards.
+        - The number of cards the player has left decreases.
+        - The turn count is updated for future reference.
+        
+        Returns:
+        The card played by the player.
         """
+        # Choose a random card from the player's hand
         _card = random.choice(self.cards)
+        
+        # Update turn number: 13 - current number of cards left gives the turn number
         turn = 13 - self.number_of_cards + 1
+        
+        # Add the played card to the player's history
         self.history.append(_card)
+        
+        # Remove the played card from the player's hand
         self.cards.remove(_card)
+        
+        # Decrease the number of cards the player has
         self.number_of_cards -= 1
 
+        # Print out the card that was played
         print(f"{self.name} played {_card.value} {_card.icon}")
+        
+        # Return the played card
         return _card
+    
+
+class Deck:
+    """
+    Deck class: Represents a deck of cards and contains methods to manipulate the deck.
+    
+    Attributes:
+    :cards: A list of all the cards in the deck.
+    
+    Methods:
+    :fill_deck(): Fills the deck with 52 cards, each having a suit and value.
+    :shuffle(): Shuffles the cards in the deck randomly.
+    :distribute(): Distributes cards to the players (4 players, 13 cards each).
+    """
+
+    # List to store the deck of cards
+    cards = []
+
+    def __init__(self):
+        """
+        Constructor for Deck class.
+        Initializes an empty deck.
+        """
+        pass
+
+    def fill_deck(self):
+        """
+        Fills the deck with 52 cards, one for each combination of suit and rank.
+        - There are 4 suits and 13 ranks, so 52 cards in total.
+        """
+        # Create a temporary list to hold the cards
+        _cards: list[Card] = []
+        
+        # Loop through suits (4 suits)
+        for i in range(0, 4):
+            # Loop through ranks (13 ranks)
+            for j in range(0, 13):
+                # Append a new card to the deck
+                _cards.append(Card(i, j))
+
+        # Set the deck's cards list to the newly created cards
+        self.cards = _cards
+
+    def shuffle(self):
+        """
+        Shuffles the cards in the deck randomly.
+        This ensures that the cards are not in a predictable order.
+        """
+        random.shuffle(self.cards)
+
+    def distribute(self, players: list[Player]):
+        """
+        Distributes 13 cards to each player.
+        - There are 4 players, each receiving 13 cards.
+        
+        Args:
+        :players: List of Player objects who will receive cards.
+        """
+        # Start index for distributing cards
+        i = 0
+        
